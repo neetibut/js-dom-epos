e5aa9c1bbbdf78f5d0732e042e32e3063cfc5e09
neetibut <neetibut@gmail.com>
2025-07-15 15:51:32 +0700

    fix(images): added correct file paths

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export function renderFooter() {
   document.querySelector("footer").innerHTML = `
       <p>Built with ❤️ using</p>
-      <img src="/javascript.svg" alt="JavaScript Logo" class="w-10 h-10" />
+      <img src="/images/javascript.svg" alt="JavaScript Logo" class="w-10 h-10" />
     `;
 }
